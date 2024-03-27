410b490c4ffffce8d64f99541d87232c6caa7496
Kkostakis <77526869+Kkostakis@users.noreply.github.com>
2024-03-27 18:05:07 +0200

    Added connection script with the database GDPR | Kkostakis

--- server.js
+++ server.js
@@ -1,11 +1,39 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+// Initialize connection to our database GDPR in Mysql via Nodejs
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+var database = require('mysql2');
+
+var mysqlconn = database.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Pas$w0rd446500!!@@##",
+  database: "GDPR",
+  port: 3306
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+mysqlconn.connect(function(error) {
+    if(error){
+        console.log("Couldn't connect :(Error: " + error);
+    } else {
+        console.log("Connected successfully to GDPR Database!!!");
+
+        var testquery = 'select * from users';
+
+        mysqlconn.query(testquery, (error,  rows) => {
+            if (error) throw error;
+            console.log(rows);
+        });
+    }    
 });
+
+
+// End the connection with our database in mysql
+/* mysqlconn.end(function(error) {
+    if (error) {
+        console.error("There was an issue with the closure process" + error.stack);
+        return;
+    }
+
+    console.log("The connection with Mysql has ended successfully!!!");
+}) */
+
+
